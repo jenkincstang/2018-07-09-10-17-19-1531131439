166b28fed84fcb66a8d65b084bf6f470be5ceadb
Jenkin Tang <jenkincs@163.com>
2020-07-23 00:06:50 +0800

    2018-07-09-10-17-19-1531131439  jenintang  test:implement the task 10 of oo homework and test it

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,24 @@
 package practice10;
 
-public class Student {
+
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass){
+        super(id, name,age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(getKlass() != null && getKlass().getLeader() != null && getKlass().getLeader().getId() == getId()){
+            return super.introduce() + " I am a Student. I am Leader of " + getKlass().getDisplayName() + ".";
+        }else{
+            return super.introduce() + " I am a Student. I am at " + getKlass().getDisplayName() + ".";
+        }
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,48 @@
 package practice10;
 
-public class Teacher {
+import java.util.List;
+
+public class Teacher extends Person {
+
+    private List<Klass> classes;
+
+    public Teacher(int id, String name, int age, List<Klass> classes){
+        super(id,name,age);
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age){
+        super(id,name,age);
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public String introduce() {
+        if(getClasses() != null && getClasses().size() == 0){
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }else {
+            return super.introduce() + " I am a Teacher. I teach " + getClasses().toString() + ".";
+        }
+
+    }
+
+    public boolean isTeaching(Student student){
+        for (Klass klass:getClasses()) {
+            if(klass.isIn(student)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String introduceWith(Student student){
+        if(isTeaching(student)){
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        }else {
+            return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+        }
+    }
 }
