0596b08d55ea2691834119c169d4b5a4df1960f9
Jenkin Tang <jenkincs@163.com>
2020-07-22 23:26:04 +0800

    2018-07-09-10-17-19-1531131439  jenintang  test:implement the task 4 of oo homework and test it

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,27 @@
 package practice04;
 
 public class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce(){
+        return  basicIntroduce();
+    }
+
+    public String basicIntroduce(){
+        return  "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
