2d84e60b673251f1c9f90b1d7d4c72d424693de3
Jenkin Tang <jenkincs@163.com>
2020-07-22 23:24:51 +0800

    2018-07-09-10-17-19-1531131439  jenintang  test:implement the task 3 of oo homework and test it

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,25 @@
 package practice02;
 
 public class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce(){
+        return  "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
 }
