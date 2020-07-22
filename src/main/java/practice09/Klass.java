daaeed136bdf4a500407e0a1776cc2e1bc4e0126
Jenkin Tang <jenkincs@163.com>
2020-07-23 00:02:21 +0800

    2018-07-09-10-17-19-1531131439  jenintang  test:implement the task 9 of oo homework and test it

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,39 @@
 package practice09;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Person leader;
+    private List<Student> students = new LinkedList<>();
+
+    public Klass(int number){
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Person getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student){
+        if (students.contains(student)){
+            this.leader = student;
+        }else {
+            System.out.println("It is not one of us.");
+        }
+
+    }
+
+    public void appendMember(Student student){
+        students.add(student);
+    }
 }
