eb5406176e693ad86e3b3cd0cfd681ed1f5a6401
Jenkin Tang <jenkincs@163.com>
2020-07-22 23:49:08 +0800

    2018-07-09-10-17-19-1531131439  jenintang  test:implement the task 8 of oo homework and test it

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,30 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Person leader;
+
+    public Klass(int number){
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Person getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student){
+        this.leader = student;
+    }
+
+    public void appendMember(Student student){
+
+    }
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,44 @@
 package practice08;
 
+import java.util.Objects;
+
 public class Person {
+    protected int id;
+    protected String name;
+    protected int age;
+
+    public Person(int id, String name,int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce(){
+        return  "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
